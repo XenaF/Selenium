bfa8008bced52744886d8fb48b27586f0ce3dfdd
Kseniya Fatsina <kseniya_fatsina@epam.com>
2021-12-06 16:51:50 +0300

    Protractor completed task

--- protractor_testing/tests/shoesCatalog.js
+++ protractor_testing/tests/shoesCatalog.js
@@ -1,13 +1,13 @@
 const { browser, by, element } = require('protractor');
-const { scrollToElement } = require('../utils')
+const { scrollToElement } = require('../utils');
  
  describe('find womens shoes in the catalog', function(){
     it('filter by women shoes', async function() {
         await browser.get('https://www.dsw.com/en/us/');
-        await browser.sleep(10000);
-        await (await element(by.xpath('//a[3]'))).click();
+        await browser.sleep(5000);
+        await element(by.xpath('//a[3]')).click();
         await element(by.xpath('//a[contains(.,"All Women\'s Boots")]')).click();
-        await browser.sleep(10000);
+        await browser.sleep(5000);
         let title = await element(by.id('result-list-toolbar-left-header'));
         expect(title.getText()).toEqual('ALL WOMEN\'S BOOTS');
     });
@@ -17,7 +17,7 @@ const { scrollToElement } = require('../utils')
         const colorFilterButton = await element(by.xpath('//*[@id="exposed-filter-color-accordion"]/label'));
         await scrollToElement(colorFilterButton);
         await colorFilterButton.click();
-        await browser.sleep(10000);
+        await browser.sleep(500);
         await element(by.xpath('//*[@id="tabpanel-exposed-filter-color"]/div/div/app-dsw-checkbox[1]/div/label/div[1]')).click();
         await browser.sleep(5000);
         let filterResult = await element.all(by.className('active-filters__filter__label'));
@@ -33,8 +33,27 @@ const { scrollToElement } = require('../utils')
         await scrollToElement(addToBagButton);
         await browser.sleep(2000);
         await addToBagButton.click();
-        await browser.sleep(3000);
+        await browser.sleep(5000);
+        await element(by.buttonText('REVIEW BAG AND CHECKOUT')).click();
+        await browser.sleep(5000);
+        let bagTitle = await element(by.className('title3 checkout-banner__title'));
+        expect(bagTitle.getText()).toEqual('MY BAG');
+        expect(element(by.className('product-module__details')).isDisplayed()).toBeTruthy();
      });
+
+
+     it('Paypal non-Angular action', async function() {
+        await browser.sleep(200);
+        await element(by.xpath('//*[@id="mainContainer"]/app-cart-checkout-routing/div/app-shopping-bag/div/div/div/div[3]/div/app-paypal-express-button/button')).click();
+        await browser.sleep(5000);
+        await browser.sleep(10000);
+        await element(by.linkText('Cancel and return to DSW')).click();
+        await browser.sleep(10000);
+        let payPalTitle = await element(by.className('title3 checkout-banner__title'));
+        await browser.sleep(300);
+        expect(payPalTitle.getText()).toEqual('MY BAG');
+     });
+
  });
 
  
