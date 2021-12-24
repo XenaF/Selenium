83ae4ce50a5c56c8e4bbb463ac66323c91eca43a
Uladzislau_Selikh <Uladzislau_Selikh@epam.com>
2021-12-24 19:07:17 +0300

    fix: correct mocha config

--- selenium_testing/test/bbcNewsSearch.js
+++ selenium_testing/test/bbcNewsSearch.js
@@ -1,17 +1,22 @@
 const {Builder,By,Key, until} = require("selenium-webdriver");
 const { expect } = require('chai');
-var webdriver = require('selenium-webdriver');
+const webdriver = require('selenium-webdriver');
+const chrome = require(`selenium-webdriver/chrome`);
+const path = require(`chromedriver`).path;
 
-async function bbcNewsSearch() {
+const service = new chrome.ServiceBuilder(path).build();
+chrome.setDefaultService(service);
 
+const driver = new webdriver.Builder()
+.withCapabilities(webdriver.Capabilities.chrome())
+.build();
 
-    beforeEach(async function() {
-        let driver = await new webdriver.Builder().forBrowser('chrome').build();
+describe('News Search', function() {
+
+beforeEach(async function() {
         await driver.manage().window().maximize();
     });
     
-
-    describe('News Search', function() {
         it('should search news by "Minsk" value', async function() {
             let globalNewsSearch = 'Minsk';
             await driver.get('https://www.euronews.com/');
@@ -21,21 +26,3 @@ async function bbcNewsSearch() {
             expect(driver.findElement(By.xpath('//h1[@class="qa-section-title o-section__title o-section__new-header__title c-section__title ""]')).to.be.equal('MINSK'));
         });
     });
-    
-
-
-   
-
-
-    // driver.executeScript('window.scrollTo(0, 5000);');
-    // driver.sleep(5000);
-    // await driver.findElement(By.xpath('//*[@id="enw-pagination-articles"]/ul/li[2]/a')).click();
-    // driver.sleep(5000);
-    // await driver.findElement(By.linkText('Belarus state airline Belavia and Turkey move to stop migrant flights')).click();
-    // driver.sleep(5000);
-    // driver.executeScript('window.scrollTo(0, 500);');
-    // driver.sleep(5000);
-    // await driver.findElement(By.xpath('//*[@id="jsMainMediaArticle-1730736"]/div[2]/div[11]/div[4]/div[2]/div[1]')).click();    
-}
-
-bbcNewsSearch();
